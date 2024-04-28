96c7047b85156f29113050fca2f78a4f6045bad5
chirandck2 <chirandck2@gmail.com>
2024-04-28 15:11:48 +0530

    firstworld

---
+++ worldbank.java
@@ -0,0 +1,23 @@
+package com.poornima;
+
+public class bank {
+
+	public static void atm() {
+		
+		System.out.println("hi, welcome to the banking");
+                 12345
+			 System.out.println(" Modified code");
+ System.out.println("From local to repository");
+		 System.out.println("From lrepo to local");
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		atm();
+		
+	}
+	
+}
